d92d63d66ea9a7c800ff8eb4b869e43e4be8d892
sgyawal1 <gyawalisuyog2@gmail.com>
2023-03-12 23:19:57 -0400

    before the button worked

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -3,9 +3,20 @@
   by adding `<script src="script.js">` just before your closing `</body>` tag
 */
 
+function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+})
+}
+
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+  const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
+  const filterDataButton = document.querySelector('.filter')
+  let currentList = [];
+
+  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
     submitEvent.preventDefault(); // This prevents your page from going to http://localhost:3000/api even if your form still has an action set on it
     console.log('form submission'); // this is substituting for a "breakpoint"
 
@@ -17,25 +28,28 @@ async function mainEvent() { // the async keyword means we can make API requests
         Let's get those form results before sending off our GET request using the Fetch API
     */
 
-    // this is the preferred way to handle form data in JS in 2022
-    const formData = new FormData(submitEvent.target); // get the data from the listener target
-    const formProps = Object.fromEntries(formData); // Turn it into an object
-
-    // You can also access all forms in a document by using the document.forms collection
-    // But this will retrieve ALL forms, not just the one that "heard" a submit event - less good
-
-    /*
-      ## Retrieving information from an API
-        The Fetch API is relatively new,
-        and is much more convenient than previous data handling methods.
-        Here we make a basic GET request to the server using the Fetch method
-        to send a request to the routes defined in /server/routes/foodServiceRoutes.js
-
       // this is a basic GET request
       // It does not include any of your form values, though
-    */
 
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+
+    currentList = await results.json();
+
+    console.table(currentList);
+  });
+
+  filterDataButton.addEventListener('click', (event) => {
+    console.log('clicked FilterButton');
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+    console.log(newList);
+
+  })
+}
     /*
    ## Get request with query parameters
 
@@ -51,12 +65,12 @@ async function mainEvent() { // the async keyword means we can make API requests
     */
 
     // This changes the response from the GET into data we can use - an "object"
-    const arrayFromJson = await results.json();
-    console.table(arrayFromJson.data); // this is called "dot notation"
+    //const arrayFromJson = await results.json();
+    //console.table(arrayFromJson.data); // this is called "dot notation"
     // arrayFromJson.data - we're accessing a key called 'data' on the returned object
     // it initially contains all 1,000 records from your request
-  });
-}
+  
+
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
