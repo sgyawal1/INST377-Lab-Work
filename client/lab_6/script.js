4d93790d6a5d89c641c421ca243cdd45b42a2c58
sgyawal1 <gyawalisuyog2@gmail.com>
2023-03-26 17:41:39 -0400

    trying to fix the null error

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -90,31 +90,5 @@ async function mainEvent() { // the async keyword means we can make API requests
     injectHTML(restaurantsList);
   })
 }
-    /*
-   ## Get request with query parameters
-
-      const results = await fetch(`/api/foodServicePG?${new URLSearchParams(formProps)}`);
-
-      The above request uses "string interpolation" to include an encoded version of your form values
-      It works because it has a ? in the string
-      Replace line 37 with it, and try it with a / instead to see what your server console says
-
-      You can check what you sent to your server in your GET request
-      By opening the "network" tab in your browser developer tools and looking at the "name" column
-      This will also show you how long it takes a request to resolve
-    */
-
-    // This changes the response from the GET into data we can use - an "object"
-    //const arrayFromJson = await results.json();
-    //console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it initially contains all 1,000 records from your request
   
-
-
-/*
-  This adds an event listener that fires our main event only once our page elements have loaded
-  The use of the async keyword means we can "await" events before continuing in our scripts
-  In this case, we load some data when the form has submitted
-*/
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
